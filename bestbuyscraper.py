94f65faa083be5d870441340bdfff4c724516de4
Anel <anel.drocic@gmail.com>
2020-11-13 16:59:46 -0600

    updated to use dotenv

--- bestbuyscraper.py
+++ bestbuyscraper.py
@@ -1,19 +1,26 @@
 import requests
-from bs4 import BeautifulSoup
 import smtplib
 import time
 import sys
+import os
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+load_dotenv()
 
 global flag
 flag = True
+
+global SLACK_WEBHOOK
+SLACK_WEBHOOK = os.environ.get('SLACK_WEBHOOK')
+
+global GOOGLE_APP_PASSWORD
+GOOGLE_APP_PASSWORD = os.environ.get('GOOGLE_APP_PASSWORD')
 
 
 def check_availability():
 
     print('\nAvailability check of the Xbox Series X on bestbuy.com')
     BEST_BUY_URL = 'https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324'
-    SLACK_WEB_HOOK = 'https://hooks.slack.com/services/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'
-
     headers = {
         "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
     slack_message = {
@@ -24,14 +31,18 @@ def check_availability():
     purchasebutton = soup.find(class_="add-to-cart-button").get_text()
 
     def send_slack():
-        requests.post(SLACK_WEB_HOOK, json=slack_message)
+        print('sending slack...')
+        print(SLACK_WEBHOOK)
+        requests.post(SLACK_WEBHOOK, json=slack_message)
 
     def send_email():
+        print(GOOGLE_APP_PASSWORD)
+        print('sending email...')
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
         server.starttls()
         server.ehlo()
-        server.login('anel.drocic@gmail.com', 'yuqwicshwguxvmxj')
+        server.login('anel.drocic@gmail.com', GOOGLE_APP_PASSWORD)
         subject = 'Xbox Available!'
         body = 'Click this link to get your Xbox! -> https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324'
         msg = f"Subject: {subject}\n\n{body}"
@@ -40,16 +51,16 @@ def check_availability():
             'anel.drocic@gmail.com',
             msg
         )
-        print('Its available! Sending email and slack now, then terminating script.\n')
         server.quit()
 
     if(purchasebutton == "Sold Out"):
-        print('-> Still sold out!')
-    else:
+        print('Its available! Sending email and slack now, then terminating script.\n')
         send_email()
         send_slack()
         global flag
         flag = False
+    else:
+        print('-> Still sold out!')
 
 
 while(flag):
