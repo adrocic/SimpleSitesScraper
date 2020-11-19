cf503b494934730b299b19d033cfd92f17a3a98b
Anel Drocic <anel.drocic@gmail.com>
2020-11-19 00:48:11 -0600

    refactored to work with multiple sites

--- bestbuyscraper.py
+++ bestbuyscraper.py
@@ -1,11 +1,14 @@
 import requests
+import settings
 import smtplib
 import time
 import sys
 import os
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+
 load_dotenv()
+settings.init()
 
 global flag
 flag = True
@@ -16,54 +19,66 @@ SLACK_WEBHOOK = os.environ.get('SLACK_WEBHOOK')
 global GOOGLE_APP_PASSWORD
 GOOGLE_APP_PASSWORD = os.environ.get('GOOGLE_APP_PASSWORD')
 
+global headers
+headers = {
+    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
+
+
+def send_email(url):
+    subject = 'Xbox Available!'
+    body = f'Click this link to get your Xbox! -> {url}'
+    msg = f"Subject: {subject}\n\n{body}"
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login('anel.drocic@gmail.com', GOOGLE_APP_PASSWORD)
+    server.sendmail(
+        'anel.drocic@gmail.com',
+        'anel.drocic@gmail.com',
+        msg
+    )
+    server.quit()
+
+
+def send_slack(url):
+    slack_message = {
+        "text": f"Hey! the Xbox Series X is available on `BestBuy.com`\n{url}"}
+    requests.post(SLACK_WEBHOOK, json=slack_message)
+
 
 def check_availability():
+    global headers
+    global purchaseButton
+    for url in settings.testers:
+        page = requests.get(url, headers=headers)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        if 'bestbuy.com' in url:
+            print('Checking BestBuy...')
+            purchaseButton = soup.find("button", text="Add to Cart")
+        elif 'walmart.com' in url:
+            print('Checking Walmart...')
+            purchaseButton = soup.find("button", text="Add to Cart")
+        elif 'target.com' in url:
+            print('Checking Target...')
+            purchaseButton = soup.find("button", text="Pick it up")
 
-    print('\nAvailability check of the Xbox Series X on bestbuy.com')
-    BEST_BUY_URL = 'https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324'
-    headers = {
-        "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
-    slack_message = {
-        "text": f"Hey! the Xbox Series X is available on `BestBuy.com`\n{BEST_BUY_URL}"}
-
-    page = requests.get(BEST_BUY_URL, headers=headers)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    purchasebutton = soup.find(class_="add-to-cart-button").get_text()
-
-    def send_slack():
-        requests.post(SLACK_WEBHOOK, json=slack_message)
-
-    def send_email():
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.ehlo()
-        server.starttls()
-        server.ehlo()
-        server.login('anel.drocic@gmail.com', GOOGLE_APP_PASSWORD)
-        subject = 'Xbox Available!'
-        body = 'Click this link to get your Xbox! -> https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324'
-        msg = f"Subject: {subject}\n\n{body}"
-        server.sendmail(
-            'anel.drocic@gmail.com',
-            'anel.drocic@gmail.com',
-            msg
-        )
-        server.quit()
-
-    if(purchasebutton == "Sold Out"):
-        print('-> Still sold out!')
-    else:
-        print('Its available! Sending email and slack now, then terminating script.\n')
-        send_email()
-        send_slack()
-        global flag
-        flag = False
+        if purchaseButton is None:
+            print('-> Still sold out!')
+        else:
+            print('Its available! Sending email and slack now, then terminating script.\n')
+            send_email(url)
+            send_slack(url)
+            global flag
+            flag = False
 
 
-while(flag):
+while flag:
     wait_time = 60 * 3
 
     check_availability()
-    if(flag):
+    if flag:
         for remaining in range(wait_time, 0, -1):
             sys.stdout.write("\r")
             sys.stdout.write(
---
+++ settings.py
@@ -0,0 +1,13 @@
+def init():
+    global sitesToScrape
+    sitesToScrape = [
+        'https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324',
+        'https://www.walmart.com/ip/XB1-Xbox-Series-X/443574645',
+        'https://www.target.com/p/xbox-series-x-console/-/A-80790841#lnk=sametab',
+    ]
+    global testers
+    testers = [
+        'https://www.bestbuy.com/site/microsoft-controller-for-xbox-series-x-xbox-series-s-and-xbox-one-latest-model-shock-blue/6430660.p?skuId=6430660',
+        'https://www.walmart.com/ip/Xbox-Wireless-Controller-Shock-Blue/265882360',
+        'https://www.target.com/p/xbox-series-x-wireless-controller-robot-white/-/A-80744785',
+    ]
